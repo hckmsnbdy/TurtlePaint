c142ef9091d8e9699ef5cc8e668b415b4c737127
hckmsnbdy <hckmsnbdy@gmail.com>
2025-11-06 14:40:30 -0500

    Wrong parameter defines corrected and shape drawing tests done in main method

--- src/main/java/com/pluralsight/MainApp.java
+++ src/main/java/com/pluralsight/MainApp.java
@@ -1,6 +1,12 @@
 package com.pluralsight;
 
+import com.pluralsight.Shapes.Circle;
+import com.pluralsight.Shapes.Hexagon;
+import com.pluralsight.Shapes.Square;
+import com.pluralsight.Shapes.Triangle;
+
 import java.awt.*;
+import java.awt.geom.Point2D;
 
 public class MainApp
 {
@@ -10,30 +16,45 @@ public class MainApp
         // the TurtleLogo application works
 
         // The world is your canvas
-        World world = new World(500, 500);
-        Turtle turtle = new Turtle(world,-100, -100);
-        
-        int width = 200;
-        int height = 200;
+        World world = new World(1000, 1000,Color.WHITE);
+        Turtle turtle = new Turtle(world,0, 0);
 
-        // calculate the hypotenuse (diagonal)
-        // a2 + b2 = c2
-        double widthSquared = Math.pow(width, 2);
-        double heightSquared = Math.pow(height, 2);
-        double hypotenuse = Math.sqrt(widthSquared + heightSquared);
+        Square square = new Square(turtle,new Point2D.Double(-280,250),Color.blue,3,150);
+        square.paint();
+
+        Triangle triangle = new Triangle(turtle, new Point2D.Double(80, -230),Color.GREEN,3, 160);
+        triangle.paint();
+
+        Circle circle = new Circle(turtle, new Point2D.Double(180, 160), Color.RED, 3,80);
+        circle.paint();
+
+        Hexagon hex = new Hexagon(turtle, new Point2D.Double(-280, -230), Color.MAGENTA, 3, 120);
+        hex.paint();
 
 
-        turtle.setPenWidth(3);
-        turtle.setColor(Color.GREEN);
+//        int width = 200;
+//        int height = 200;
+//
+//        // calculate the hypotenuse (diagonal)
+//        // a2 + b2 = c2
+//        double widthSquared = Math.pow(width, 2);
+//        double heightSquared = Math.pow(height, 2);
+//        double hypotenuse = Math.sqrt(widthSquared + heightSquared);
+//
+//
+//        turtle.setPenWidth(3);
+//        turtle.setColor(Color.GREEN);
+//
+//        turtle.turnRight(45);
+//        turtle.forward(hypotenuse);
+//
+//        turtle.penUp();
+//        turtle.goTo(100, 100);
+//        turtle.turnRight(90);
+//
+//        turtle.penDown();
+//        turtle.forward(hypotenuse);
 
-        turtle.turnRight(45);
-        turtle.forward(hypotenuse);
 
-        turtle.penUp();
-        turtle.goTo(100, 100);
-        turtle.turnRight(90);
-
-        turtle.penDown();
-        turtle.forward(hypotenuse);
     }
 }
--- src/main/java/com/pluralsight/Shapes/Square.java
+++ src/main/java/com/pluralsight/Shapes/Square.java
@@ -4,11 +4,12 @@ import com.pluralsight.Shape;
 import com.pluralsight.Turtle;
 
 import java.awt.*;
+import java.awt.geom.Point2D;
 
 public class Square extends Shape {
     private double size;
 
-    public Square(Turtle turtle, Point location, Color borderColor, int borderWidth, double size) {
+    public Square(Turtle turtle, Point2D location, Color borderColor, int borderWidth, double size) {
         super(turtle, location, borderColor, borderWidth);
         this.size = size;
     }
@@ -20,7 +21,7 @@ public class Square extends Shape {
 
         jumpTo(location);
         turtle.setHeading(0);
-        for (int i = 1; i < 4; i++) {
+        for (int i = 0; i < 4; i++) {
             turtle.forward(size);
             turtle.turnRight(90);
         }
