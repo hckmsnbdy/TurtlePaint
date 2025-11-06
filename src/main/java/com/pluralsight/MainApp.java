b623e3f3e9a4a8c020d0dca3cea2fd14eb89fa1c
hckmsnbdy <hckmsnbdy@gmail.com>
2025-11-06 15:15:40 -0500

    Created:  -Menu and draw shape methods

--- src/main/java/com/pluralsight/MainApp.java
+++ src/main/java/com/pluralsight/MainApp.java
@@ -7,54 +7,215 @@ import com.pluralsight.Shapes.Triangle;
 
 import java.awt.*;
 import java.awt.geom.Point2D;
+import java.util.Scanner;
 
 public class MainApp
 {
     public static void main(String[] args)
     {
-        // This starter code to get you familiar with how
-        // the TurtleLogo application works
 
-        // The world is your canvas
-        World world = new World(1000, 1000,Color.WHITE);
-        Turtle turtle = new Turtle(world,0, 0);
+        Scanner in = new Scanner(System.in);
 
-        Square square = new Square(turtle,new Point2D.Double(-280,250),Color.blue,3,150);
-        square.paint();
+        System.out.println("=== Turtle Painter ===");
 
-        Triangle triangle = new Triangle(turtle, new Point2D.Double(80, -230),Color.GREEN,3, 160);
-        triangle.paint();
+        System.out.print("Enter world width: ");
+        int worldW = Integer.parseInt(in.nextLine());
 
-        Circle circle = new Circle(turtle, new Point2D.Double(180, 160), Color.RED, 3,80);
-        circle.paint();
+        System.out.print("Enter world height: ");
+        int worldH = Integer.parseInt(in.nextLine());
 
-        Hexagon hex = new Hexagon(turtle, new Point2D.Double(-280, -230), Color.MAGENTA, 3, 120);
-        hex.paint();
+        boolean running = true;
 
+        World world = new World(worldW, worldH);
+        Turtle turtle = new Turtle(world, 0, 0);
 
-//        int width = 200;
-//        int height = 200;
-//
-//        // calculate the hypotenuse (diagonal)
-//        // a2 + b2 = c2
-//        double widthSquared = Math.pow(width, 2);
-//        double heightSquared = Math.pow(height, 2);
-//        double hypotenuse = Math.sqrt(widthSquared + heightSquared);
-//
-//
-//        turtle.setPenWidth(3);
-//        turtle.setColor(Color.GREEN);
-//
-//        turtle.turnRight(45);
-//        turtle.forward(hypotenuse);
-//
-//        turtle.penUp();
-//        turtle.goTo(100, 100);
-//        turtle.turnRight(90);
-//
-//        turtle.penDown();
-//        turtle.forward(hypotenuse);
+        while (running)
+        {
+            System.out.println();
+            System.out.println("Home Screen");
+            System.out.println("1) Add Shape");
+            System.out.println("2) Save Image");
+            System.out.println("0) Exit");
 
+            System.out.print("Choose: ");
+            String input = in.nextLine();
 
+            switch (input)
+            {
+                case "1":
+                    System.out.println("Add Shape selected.");
+                    addShape(in, turtle);
+                    break;
+
+                case "2":
+                    System.out.println("Save Image option selected.");
+                    // save method
+                    break;
+
+                case "0":
+                    System.out.println("Exiting program...");
+                    running = false;
+                    break;
+
+                default:
+                    System.out.println("Invalid choice, try again.");
+            }
+        }
+
+        in.close();
     }
+
+    private static void addShape(Scanner in, Turtle turtle)
+    {
+        System.out.println();
+        System.out.println("Choose a shape:");
+        System.out.println("1) Square");
+        System.out.println("2) Circle");
+        System.out.println("3) Triangle");
+        System.out.println("4) Hexagon");
+        System.out.println("0) Cancel");
+
+        System.out.print("Choice: ");
+        String choice = in.nextLine();
+
+        switch (choice)
+        {
+            case "1":
+                drawSquare(in, turtle);
+                break;
+
+            case "2":
+                drawCircle(in, turtle);
+                break;
+
+            case "3":
+                drawTriangle(in, turtle);
+                break;
+
+            case "4":
+                drawHexagon(in, turtle);
+                break;
+
+            case "0":
+                System.out.println("Cancelled.");
+                break;
+
+            default:
+                System.out.println("Invalid choice.");
+        }
+    }
+
+    private static void drawSquare(Scanner in, Turtle turtle)
+    {
+        System.out.println("-- Square --");
+
+        // border color
+        System.out.print("Border color name (red, blue, etc): ");
+        String colorName = in.nextLine();
+        Color color = getColor(colorName);
+
+        // border width
+        System.out.print("Border width (int): ");
+        int border = Integer.parseInt(in.nextLine());
+
+        // location
+        System.out.print("Top-left X: ");
+        double x = Double.parseDouble(in.nextLine());
+
+        System.out.print("Top-left Y: ");
+        double y = Double.parseDouble(in.nextLine());
+
+        // size
+        System.out.print("Edge length: ");
+        double size = Double.parseDouble(in.nextLine());
+
+        Square s = new Square(turtle, new Point2D.Double(x, y), color, border, size);
+        s.paint();
+    }
+
+    private static void drawCircle(Scanner in, Turtle turtle)
+    {
+        System.out.println("-- Circle --");
+
+        System.out.print("Border color: ");
+        String colorName = in.nextLine();
+        Color color = getColor(colorName);
+
+        System.out.print("Border width: ");
+        int border = Integer.parseInt(in.nextLine());
+
+        System.out.print("Center X: ");
+        double x = Double.parseDouble(in.nextLine());
+
+        System.out.print("Center Y: ");
+        double y = Double.parseDouble(in.nextLine());
+
+        System.out.print("Radius: ");
+        double radius = Double.parseDouble(in.nextLine());
+
+        Circle c = new Circle(turtle, new Point2D.Double(x, y), color, border, radius);
+        c.paint();
+    }
+
+    private static void drawTriangle(Scanner in, Turtle turtle)
+    {
+        System.out.println("-- Triangle --");
+
+        System.out.print("Border color: ");
+        String colorName = in.nextLine();
+        Color color = getColor(colorName);
+
+        System.out.print("Border width: ");
+        int border = Integer.parseInt(in.nextLine());
+
+        System.out.print("First vertex X: ");
+        double x = Double.parseDouble(in.nextLine());
+
+        System.out.print("First vertex Y: ");
+        double y = Double.parseDouble(in.nextLine());
+
+        System.out.print("Side length: ");
+        double side = Double.parseDouble(in.nextLine());
+
+        Triangle t1 = new Triangle(turtle, new Point2D.Double(x, y), color, border, side);
+        t1.paint();
+    }
+    private static void drawHexagon(Scanner in, Turtle turtle)
+    {
+        System.out.println("-- Hexagon --");
+
+        System.out.print("Border color: ");
+        String colorName = in.nextLine();
+        Color color = getColor(colorName);
+
+        System.out.print("Border width: ");
+        int border = Integer.parseInt(in.nextLine());
+
+        System.out.print("First vertex X: ");
+        double x = Double.parseDouble(in.nextLine());
+
+        System.out.print("First vertex Y: ");
+        double y = Double.parseDouble(in.nextLine());
+
+        System.out.print("Side length: ");
+        double side = Double.parseDouble(in.nextLine());
+
+        Hexagon h = new Hexagon(turtle, new Point2D.Double(x, y), color, border, side);
+        h.paint();
+    }
+    private static Color getColor(String name)
+    {
+        name = name.toLowerCase();
+
+        if (name.equals("red")) return Color.RED;
+        if (name.equals("green")) return Color.GREEN;
+        if (name.equals("blue")) return Color.BLUE;
+        if (name.equals("black")) return Color.BLACK;
+        if (name.equals("white")) return Color.WHITE;
+        if (name.equals("yellow")) return Color.YELLOW;
+        if (name.equals("magenta")) return Color.MAGENTA;
+
+        return Color.BLACK;
+    }
+
 }
