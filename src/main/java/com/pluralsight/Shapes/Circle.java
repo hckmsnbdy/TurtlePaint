4f480da9ef093cceed119a1dc311b17fd78fcfa5
hckmsnbdy <hckmsnbdy@gmail.com>
2025-11-06 14:04:42 -0500

    Added:  -Overrided paint() method in Circle class to draw a Circle.

--- src/main/java/com/pluralsight/Shapes/Circle.java
+++ src/main/java/com/pluralsight/Shapes/Circle.java
@@ -1,7 +1,40 @@
 package com.pluralsight.Shapes;
 
 import com.pluralsight.Shape;
+import com.pluralsight.Turtle;
+
+import java.awt.*;
+import java.awt.geom.Point2D;
 
 public class Circle extends Shape {
+    private double radius;
 
+    public Circle(Turtle turtle, Point2D location, Color borderColor, int borderWidth, double radius) {
+        super(turtle, location, borderColor, borderWidth);
+        this.radius = radius;
+    }
+
+    @Override
+    public void paint() {
+        prepPen();
+
+        double  x1 = location.getX() + radius;
+        double y1 = location.getY();
+
+        jumpTo(x1, y1);
+
+        int steps = 72;
+        double turnAngle = 360/steps;
+
+        double segmentLength = (2*Math.PI*radius)/steps;
+
+        turtle.setHeading(90);
+
+        for (int i = 0; i <= steps; i++) {
+            turtle.turnRight(turnAngle);
+            turtle.forward(segmentLength);
+        }
+
+
+    }
 }
