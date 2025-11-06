59b4a6990015a7146e16c04a33a8076acb8efb36
hckmsnbdy <hckmsnbdy@gmail.com>
2025-11-06 14:19:58 -0500

    Added:  -Overrided paint() method in Hexagon class to draw a Hexagon.

--- src/main/java/com/pluralsight/Shapes/Hexagon.java
+++ src/main/java/com/pluralsight/Shapes/Hexagon.java
@@ -1,6 +1,27 @@
 package com.pluralsight.Shapes;
 
 import com.pluralsight.Shape;
+import com.pluralsight.Turtle;
+
+import java.awt.*;
+import java.awt.geom.Point2D;
 
 public class Hexagon extends Shape {
+    private double side;
+
+    public Hexagon(Turtle turtle, Point2D location, Color borderColor, int borderWidth, double side) {
+        super(turtle, location, borderColor, borderWidth);
+        this.side = side;
+    }
+
+    @Override
+    public void paint() {
+        prepPen();
+        jumpTo(location);
+        turtle.setHeading(0);
+        for (int i = 0; i < 6; i++) {
+            turtle.forward(side);
+            turtle.turnRight(60);
+        }
+    }
 }
